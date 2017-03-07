50b9f6feaaadce837e3a16816eb88c98244900cd
Gilles B <bonhoure.gilles@gmail.com>
2017-03-07 14:10:26 +0100

    quelques tests

--- web-app/client/main.js
+++ web-app/client/main.js
@@ -5,4 +5,6 @@ Router.route('/', function () {
   this.render('Home');
 });
 
-
+Router.route('/register', function () {
+  this.render('Register');
+});
--- web-app/imports/ui/connect.js
+++ web-app/imports/ui/connect.js
@@ -1,11 +1,24 @@
 import { Template } from 'meteor/templating';
- 
-import './welcome.html';
- 
+
+import './connect.html';
+
 Template.body.helpers({
   tasks: [
     { text: 'This is task 1' },
     { text: 'This is task 2' },
     { text: 'This is task 3' },
   ],
+});
+
+Template.register.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        Accounts.createUser({
+            email: email,
+            password: password
+        });
+        Router.go('home');
+    }
 });
