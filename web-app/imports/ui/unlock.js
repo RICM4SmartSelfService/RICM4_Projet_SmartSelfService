0f6213544f9217a8d766663f18c9df2344c00090
Talenie <alicia.abonnenc@hotmail.com>
2017-03-13 18:31:24 +0100

    Added error message when wrong code is given

--- web-app/imports/ui/unlock.js
+++ web-app/imports/ui/unlock.js
@@ -1,9 +1,15 @@
 import { Template } from 'meteor/templating';
 import './unlock.html';
 import { Lockers } from '../api/lockers.js';
-import { Session } from 'meteor/session';
+//import { Session } from 'meteor/session';
 
 var locker;
+var error;
+
+Template.unlock.onCreated(function() {
+  this.lastError = new ReactiveVar(null);
+});
+
 
 Template.unlock.helpers({
   locker() { 
@@ -11,13 +17,20 @@ Template.unlock.helpers({
 	locker = Lockers.findOne({_id: new Mongo.ObjectID(id)});
 	return locker;
   },
+  errorMessage : function() {
+    return Template.instance().lastError.get();
+  }
 });
 
 Template.unlock.events({
-	'submit .codeForm' : function(event) {
+	'submit .codeForm' : function(event, template) {
 		event.preventDefault();
+		// If the code is the right one unlock, else display error message
 		if(event.target.code.value.localeCompare(locker.code)==0){
 			console.log("Correct");
+			template.lastError.set(null);
+		} else {
+			template.lastError.set("Wrong code");
 		}
 	},
 });
