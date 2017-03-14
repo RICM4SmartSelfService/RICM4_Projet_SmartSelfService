6c4f19e56eae7b8b3a9549a8d6e4c6a9ba93dfd5
Talenie <alicia.abonnenc@hotmail.com>
2017-03-14 16:11:57 +0100

    Fidlled with user in home.js

--- web-app/imports/ui/home.js
+++ web-app/imports/ui/home.js
@@ -2,4 +2,17 @@ import { Meteor } from 'meteor/meteor'
 import { Accounts } from 'meteor/accounts-base'
 import './home.html';
 
-console.log(Meteor.user());
+
+Template.home.onCreated (function() {
+		this.user = new ReactiveVar(null);
+	}
+);
+
+
+Template.home.helpers({
+	myCurrentUser(){
+		Template.instance().user = Meteor.user();
+		console.log(Template.instance().user);
+		return Meteor.user();
+	}
+});
