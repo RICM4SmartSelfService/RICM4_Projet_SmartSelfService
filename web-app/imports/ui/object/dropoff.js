8bfbf5f5d62a155b4353da96691649e4fbdd1753
Gilles B <bonhoure.gilles@gmail.com>
2017-03-20 16:05:10 +0100

    drop-off update

--- web-app/imports/ui/object/dropoff.js
+++ web-app/imports/ui/object/dropoff.js
@@ -3,7 +3,11 @@ import { Template } from 'meteor/templating';
 import './dropoff.html';
 
 Template.dropOffObject.rendered = function(){
-    $('.datetimepicker').datetimepicker();
+    $('.datetimepicker').datetimepicker({
+      format:'MM-DD-YYYY HH:mm:ss'
+    });
+
+    //console.log(Router.current().params);
 };
 
 Template.dropOffObject.events({
@@ -13,6 +17,14 @@ Template.dropOffObject.events({
     var title = $('[name=title]').val();
     var description = $('[name=description]').val();
     var limitDate = $('[name=limitDate]').val();
+
+    var d_limit = new Date(limitDate).getTime();
+
+    if(title != "" && description != "" && d_limit > Date.now()) {
+
+    } else {
+      alert("Champs non remplis ou date incorrecte (rentrez une date future).");
+    }
 
 
   }
