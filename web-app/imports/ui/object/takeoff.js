781e2d837c85249796fb48cf622252dbd14f1bfd
Talenie <alicia.abonnenc@hotmail.com>
2017-03-20 17:09:43 +0100

    WIP : updating the DB when locker opened

--- web-app/imports/ui/object/takeoff.js
+++ web-app/imports/ui/object/takeoff.js
@@ -1,6 +1,7 @@
 import { Template } from 'meteor/templating';
 import './takeoff.html';
 import { Lockers } from '../../api/lockers.js';
+import { Accounts } from 'meteor/accounts-base';
 
 Template.takeoff.helpers({
 	locker() {
@@ -18,11 +19,26 @@ Template.reserved.helpers({
 
 Template.takeoff.events({
 	'click .Reserve' : function(event){
-		console.log(event.target.id);
+		// Getting the lockers id
 		var id = event.target.id;
-		Lockers.update({_id : id}, $set : [{available : false}]);
+		Lockers.update(id, {
+			$set: {available : false, "pending" : "take"},
+		});
 		
+		// Adding the code into teh pending actions of the user
+		var locker = Lockers.findOne({_id : id});
+		var IDuser = Meteor.userId();
+		Accounts.users.update(IDuser, {
+			{ $push : { 
+				"actions" : {
+					"type" : "take",
+					"locker" : id,
+					"code" :  locker.code,
+				}
+			}}
+		});
 		
+		// Going to the next page		
 		Router.go('/takeoff/'+id+'/reserved');
 	},
 });
--- web-app/imports/ui/unlock.js
+++ web-app/imports/ui/unlock.js
@@ -31,8 +31,24 @@ Template.unlock.events({
 		// If the code is the right one unlock, else display error message
 		if(event.target.code.value.localeCompare(locker.code)==0){
 			template.success.set(true);
-			console.log("Correct");
 			template.lastError.set(null);
+			
+			var id = Router.current().params.id;
+			var locker = Lockers.findOne({_id: id});
+			
+			// Updating the locker
+			if(locker.pending == "take"){
+				
+			} else if (locker.pending == "drop"){
+				
+			}
+			
+			var newcode =(Math.floor(Math.random() * 1000)).toString();
+			Lockers.update(id, {
+				$set : {"code" : newcode}
+			});
+			
+			
 		} else {
 			template.lastError.set("Wrong code");
 		}
