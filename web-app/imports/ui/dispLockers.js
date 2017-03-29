5694c1f1bff1be802bc7400edc58d073aadd9146
Talenie <alicia.abonnenc@hotmail.com>
2017-03-29 11:05:10 +0200

    added locker display for admins

---
+++ web-app/imports/ui/dispLockers.js
@@ -0,0 +1,46 @@
+import { Template } from 'meteor/templating';
+import './dispLockers.html';
+import { Lockers } from '../api/lockers.js';
+import { Objects } from '../api/objects.js';
+import { Session } from 'meteor/session';
+
+
+Template.dispLockers.helpers({
+  lockers() {
+	var regexp = new RegExp(Session.get('search/keyword'), 'i');
+	
+	// Returns the id of all the objects which names are in the regex
+	var objects_id = Objects.find({ name : regexp }).map(function(a){return a._id;});
+	
+	
+	// Searches in the locker name, number and content but only if it has an object that is pickable (available)
+	return Lockers.find({
+		 $or : [ // Search for either one of the 3
+			{place : regexp},
+			{number : regexp}, 
+			{object : { $in : objects_id } } // Object with name
+	]});
+  },
+});
+
+Template.search.events({
+  'submit .query': function(event) {
+	// Prevents the browser from recharging
+	event.preventDefault();
+	// Puts the query in the session
+	Session.set('search/keyword', event.target.text.value);
+
+  },
+  // For researching as soon as the input changes
+  'keyup .queryTerm': function(event) {
+	// Puts the query in the session
+	Session.set('search/keyword', event.target.value);
+	},
+	// When we click on the takeoff button, takes you to the next page for confimation
+  'click .Goto' : function(event){
+	console.log(event.target.id);
+	var id = event.target.id;
+	Router.go('/takeoff/'+id);
+  },
+});
+
