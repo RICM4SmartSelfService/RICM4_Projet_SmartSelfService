6e29e8c36742b69bce55fb2bdc4d07755f2765bc
Talenie <alicia.abonnenc@hotmail.com>
2017-04-02 21:34:39 +0200

    removed unused folders, added code to put into the ESP8266 chips and added documentation folder

---
+++ ESP8266/ESP8266_SSS_cloud.ino
@@ -0,0 +1,76 @@
+/*
+  This a simple example of the aREST Library for the ESP8266 WiFi chip.
+  This example illustrate the cloud part of aREST that makes the board accessible from anywhere
+
+  Written in 2015 by Marco Schwartz under a GPL license.
+  Modified in 2017 by Alicia Abonnenc
+*/
+
+// Import required libraries
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <aREST.h>
+
+// Clients
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// Create aREST instance
+aREST rest = aREST(client);
+
+// Unique ID to identify the device for cloud.arest.io
+char* device_id = "SSS01";
+
+// WiFi parameters
+const char* ssid = "your_wifi_network_name";
+const char* password = "your_wifi_network_password";
+
+// Variables to be exposed to the API
+int contact;
+
+// Functions
+void callback(char* topic, byte* payload, unsigned int length);
+
+void setup(void)
+{
+  // Start Serial
+  Serial.begin(115200);
+
+  // Set callback
+  client.setCallback(callback);
+
+  // Init variables and expose them to REST API
+  contact = true;
+  rest.variable("contact",&temperature);
+
+  // Give name & ID to the device (ID should be 6 characters long)
+  rest.set_id(device_id);
+  rest.set_name("esp8266");
+
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  // Set output topic
+  char* out_topic = rest.get_topic();
+
+}
+
+void loop() {
+
+  // Connect to the cloud
+  rest.handle(client);
+
+}
+
+// Handles message arrived on subscribed topic(s)
+void callback(char* topic, byte* payload, unsigned int length) {
+
+  rest.handle_callback(client, topic, payload, length);
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RICM4_Projet_SmartSelfService
-Projet de 4eme année, d'étudiants en RICM à Polytech Grenoble, pour piloter des casiers connectés.
+Project made by students of Polytech Grenoble in the field RICM (Network, Computer Science and Communication) in their fourth year.
+This project aims to be a platform to control connected lockers.
+
+
